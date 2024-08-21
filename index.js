4c67d4892a7f072af5731375defb8ef8eeeb612c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2024-08-21 15:25:48 +0530

    Add resolvers for GraphQL queries

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import typeDefs from "./schema";
+import db from "./_db";
 
+const resolvers = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+  },
+};
 // setup ApolloServer
 const server = new ApolloServer({
   // typeDefs and resolvers
   typeDefs,
+  resolvers,
 });
 
 const { url } = await startStandaloneServer({ server, port: 4000 });
