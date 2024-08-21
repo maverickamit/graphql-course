b5d1c0ac9615a600b0037099fbc5d8070465dcea
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2024-08-21 14:28:23 +0530

    Add GraphQL typedefs in schema.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import typeDefs from "./schema";
 
 // setup ApolloServer
 const server = new ApolloServer({
   // typeDefs and resolvers
+  typeDefs,
 });
 
 const { url } = await startStandaloneServer({ server, port: 4000 });
